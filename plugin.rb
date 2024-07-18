dee9d4ccbdda5c61f290b727b26d85ae17c787f5
Roy Scheeren <hi@royscheeren.com>
2024-07-18 13:46:26 +0200

    refactor: remove license from plugin.rb
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,3 @@
-#
-# SPDX-FileCopyrightText: Copyright (C) 2022, vDL Digital Ventures GmbH <info@vdl.digital>
-# SPDX-FileCopyrightText: Copyright (C) 2021, Spruce Systems, Inc.
-#
-# SPDX-License-Identifier: MIT
-#
-
 # frozen_string_literal: true
 
 # name: discourse-siwt
